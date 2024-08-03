86889de63f8a820c6f3e6432812825dae9bcffbe
EmreCanTERKAN <emrecanterkan06@gmail.com>
2024-08-03 18:36:58 +0300

    Add project files.

---
+++ Week4-Inheritance/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        Teacher teacher = new()
+        {
+            Name = "Emre",
+            LastName = "TERKAN",
+            Salary = 5000
+        };
+        teacher.Info();
+        Student student = new()
+        {
+            Name = "Can",
+            LastName = "TERKAN",
+            studentNumber = 10
+        };
+        student.Info();
+
+    }
+
+}
+abstract class Person
+{
+    public string Name { get; set; }
+    public string LastName { get; set; }
+    abstract public void Info();
+
+}
+
+class Student : Person
+{
+    public int studentNumber { get; set; }
+
+    public override void Info()
+    {
+        Console.WriteLine($"Öğrencinin:\nAdı : {Name}\nSoyadı : {LastName}\nÖğrenci Numarası : {studentNumber}");
+    }
+}
+
+class Teacher : Person
+{
+    public decimal Salary { get; set; }
+    public override void Info()
+    {
+        Console.WriteLine($"Öğretmenin:\nAdı : {Name}\nSoyadı : {LastName}\nMaaş Bilgisi : {Salary}");
+    }
+}
